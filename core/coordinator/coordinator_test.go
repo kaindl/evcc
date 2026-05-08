3d8d3a12bf21a50ea507201bca136fa4e7f08dac
Markus Kaindl <github@markus-kaindl.de>
2026-05-09 01:22:35 +0200

    test: added vehicle detection tests for autodiscoverable flag

--- core/coordinator/coordinator_test.go
+++ core/coordinator/coordinator_test.go
@@ -22,16 +22,20 @@ func TestVehicleDetectByStatus(t *testing.T) {
 
 	type testcase struct {
 		string
-		v1, v2 api.ChargeStatus
-		res    api.Vehicle
+		v1, v2             api.ChargeStatus
+		v2autodiscoverable bool
+		res                api.Vehicle
 	}
 	tc := []testcase{
-		{"A/A->0", api.StatusA, api.StatusA, nil},
-		{"B/A->1", api.StatusB, api.StatusA, v1},
-		{"B/A->1", api.StatusB, api.StatusA, v1},
-		{"A/B->2", api.StatusA, api.StatusB, v2},
-		{"A/B->2", api.StatusA, api.StatusB, v2},
-		{"B/B->1", api.StatusB, api.StatusB, nil},
+		{"A/A->0", api.StatusA, api.StatusA, true, nil},
+		{"B/A->1", api.StatusB, api.StatusA, true, v1},
+		{"B/A->1", api.StatusB, api.StatusA, true, v1},
+		{"A/B->2", api.StatusA, api.StatusB, true, v2},
+		{"A/B->2", api.StatusA, api.StatusB, true, v2},
+		{"A/B->2", api.StatusA, api.StatusB, false, nil},
+		{"A/B->2", api.StatusA, api.StatusB, false, nil},
+		{"B/B->1", api.StatusB, api.StatusB, true, nil},
+		{"B/B->1", api.StatusB, api.StatusB, false, v1},
 	}
 
 	log := util.NewLogger("foo")
@@ -42,7 +46,6 @@ func TestVehicleDetectByStatus(t *testing.T) {
 	v1.MockVehicle.EXPECT().Identifiers().Return(nil).AnyTimes()
 	v2.MockVehicle.EXPECT().Identifiers().Return([]string{"it's me"}).AnyTimes()
 	v1.MockVehicle.EXPECT().AutoDiscoveryEnabled().Return(true).AnyTimes()
-	v2.MockVehicle.EXPECT().AutoDiscoveryEnabled().Return(true).AnyTimes()
 
 	var lp loadpoint.API
 	c := New(log, vehicles)
@@ -51,7 +54,10 @@ func TestVehicleDetectByStatus(t *testing.T) {
 		t.Logf("%+v", tc)
 
 		v1.MockChargeState.EXPECT().Status().Return(tc.v1, nil)
-		v2.MockChargeState.EXPECT().Status().Return(tc.v2, nil)
+		v2.MockVehicle.EXPECT().AutoDiscoveryEnabled().Return(tc.v2autodiscoverable)
+		if tc.v2autodiscoverable {
+			v2.MockChargeState.EXPECT().Status().Return(tc.v2, nil)
+		}
 
 		available := c.availableDetectibleVehicles(lp) // include id-able vehicles
 		res := c.identifyVehicleByStatus(available)
